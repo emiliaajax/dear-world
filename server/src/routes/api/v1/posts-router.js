ac2e468596ea9f42af165d49c1c15ff19352f186
Emilia Hansson <eh222yn@student.lnu.se>
2022-10-17 19:59:52 +0200

    Add router folder and file and start creating routes

---
+++ server/src/routes/api/v1/posts-router.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import { PostsController } from '../../../controllers/api/posts-controller.js'
+
+export const router = express.Router()
+
+const controller = new PostsController()
+
+router.get('/',
+  (req, res, next) => controller.findAllPosts(req, res, next)
+)
+
+router.get('/:id',
+  (req, res, next) => controller.findPost(req, res, next, id)
+)
