9e49508595c9d90e8c315db79df607ca98f3c167
Emilia Hansson <eh222yn@student.lnu.se>
2022-10-20 18:30:27 +0200

    Add missing try block

--- server/src/controllers/api/comments-controller.js
+++ server/src/controllers/api/comments-controller.js
@@ -2,12 +2,14 @@ import { Comment } from '../../models/comment.js'
 
 export class CommentsController {
   async findPostComments (req, res, next) {
-    const comments = await Comment.find({ postId: req.body.postId })
+    try {
+      const comments = await Comment.find({ postId: req.body.postId })
 
-    res
-      .status(200)
-      .json(comments)
-  } catch (error) {
-    next(error)
+      res
+        .status(200)
+        .json(comments)
+    } catch (error) {
+      next(error)
+    }
   }
 }
