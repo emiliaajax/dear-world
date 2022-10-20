9f2c63450cc1684711090a0cd405a6559238dc59
Emilia Hansson <eh222yn@student.lnu.se>
2022-10-20 18:29:32 +0200

    Add comments controller and implement findPostComments method

---
+++ server/src/controllers/api/comments-controller.js
@@ -0,0 +1,13 @@
+import { Comment } from '../../models/comment.js'
+
+export class CommentsController {
+  async findPostComments (req, res, next) {
+    const comments = await Comment.find({ postId: req.body.postId })
+
+    res
+      .status(200)
+      .json(comments)
+  } catch (error) {
+    next(error)
+  }
+}
